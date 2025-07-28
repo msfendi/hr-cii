6a9da05ecbe567439398a76be2e36ae420d23e7c
msfendi <efendisyarifudin1@gmail.com>
2025-07-28 13:30:18 +0700

    initial commit

---
+++ resources/views/layout/sidebar.blade.php
@@ -0,0 +1,48 @@
+<!-- Sidebar -->
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+    <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center">
+        <div class="sidebar-brand-icon">
+            <img src="{{ asset('img/chutex.svg') }}" style="width: 40px;">
+        </div>
+        <div class="sidebar-brand-text mx-3">Chutex <sup>Sys</sup></div>
+    </a>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider my-0">
+
+    <!-- Nav Item - Dashboard -->
+    <li class="nav-item active">
+        <a class="nav-link" href="{{ route('home') }}">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard</span></a>
+    </li>
+
+    @if($roleusers[0]->rolename == 'Admin')
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUser"
+            aria-expanded="true" aria-controls="collapseUser">
+            <i class="fas fa-fw fa-users"></i>
+            <span>User</span>
+        </a>
+        <div id="collapseUser" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <a class="collapse-item" href="{{ route('user.index') }}">Daftar User</a>
+                <a class="collapse-item" href="{{ route('role.index') }}">Daftar Role</a>
+            </div>
+        </div>
+    </li>
+    @endif
+
+
+    <!-- Divider -->
+    <hr class="sidebar-divider d-none d-md-block">
+
+    <!-- Sidebar Toggler (Sidebar) -->
+    <!-- <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div> -->
+
+</ul>
+<!-- End of Sidebar -->
