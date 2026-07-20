a8729b85b7b6954a77c0a4b4d3508393cd4449c0
Dimas Galang <dimasgalang@student.com>
2026-07-20 15:06:27 +0700

    fix: database smartit connection

--- config/database.php
+++ config/database.php
@@ -139,7 +139,7 @@ return [
             'charset'         => 'utf8',
             'prefix'          => '',
             'prefix_indexes'  => true,
-            'encrypt' => 'false',
+            'encrypt' => false,
             'trust_server_certificate' => true,
             // 'trust_server_certificate' => true, // aktifkan kalau SQL Server pakai self-signed cert
         ],
